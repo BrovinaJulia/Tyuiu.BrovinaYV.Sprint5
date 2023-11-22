feca1ec5b438c689a404ed6f5a3c208292c23fc5
Юлия Бровина <Tyuiu.BrovinaYV@outlook.com>
2023-11-22 15:54:08 +0500

    Task4

---
+++ Tyuiu.BrovinaYV.Sprint5.Task4.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BrovinaYV.Sprint5.Task4.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BrovinaYV.Sprint5.Task4.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fa70132f-3431-4cc2-8624-83d76b60be92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BrovinaYV.Sprint5.Task4.V8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BrovinaYV.Sprint5.Task4.V8.Lib;
+using System.IO;
+
+namespace Tyuiu.BrovinaYV.Sprint5.Task4.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Бровина Ю. В. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант 8                                                               *");
+            Console.WriteLine("* Выполнила: Бровина Юлия Викторовна | АСОиУб-23-2                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Прочитать значение из файла и подставить вместо Х в формуле. Вычислить  *");
+            Console.WriteLine("* значение по формуле и вернуть полученный результат на консоль.          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask4V8.txt";
+           
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
